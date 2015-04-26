bc2ef513af52c34ab721a1a3f4f12982834b27af
jcskyting <jcskyting@gmail.com>
2015-04-27 05:21:16 +0800

    change like to Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,48 @@
 class LikesController < ApplicationController
+  before_action :set_photo
+
   def create
     @photos = Photo.all
-    photo = Photo.find(params[:photo_id])
+    @photo = Photo.find(params[:photo_id])
 
-    if photo.find_like_by_user(current_user)
+    if @photo.find_like_by_user(current_user)
     else
-      Like.create(:photo =>photo, :user => current_user)
+      Like.create(:photo => @photo, :user => current_user)
     end
-    redirect_to root_url
+    # redirect_to root_url
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js {
+        @photo.reload
+        render :template => "likes/reload"
+      }
+    end
+
   end
 
   def destroy
-    @photos = Photo.all
+
     like = current_user.likes.find(params[:id])
     if like
       like.destroy
     end
-    redirect_to root_url
+    # redirect_to root_url
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js {
+        @photo.reload
+        render :template => "likes/reload"
+      }
+    end
+
   end
 
   private
 
-
+  def set_photo
+    @photos = Photo.all
+    @photo = Photo.find(params[:photo_id])
+  end
 end
